9be919bdc74837f659d64cd7de52716c72a6dfae
Sergio <webdev.slara@gmail.com>
2022-05-11 12:17:38 +0200

    setting up modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+//MODULE TYPES:
+/* Domain: wraps up all the components needed to implement one single feat.
+   Routed: same as Domain but these components are tied to a specific route.
+   Routing: Defines routing rules (show x component when a user is at a route '/all' for example.)
+   Service: Deifines services that will be used in multiple parts of the app
+   Widget: Defines reusable components that will be used in other components
+*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
