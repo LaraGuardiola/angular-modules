7a4f94fd33d8d8b36770ff37e0aef19425792da1
Sergio <webdev.slara@gmail.com>
2022-05-12 17:26:43 +0200

    created table component with structural directive using parent property, also classes

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    { name: 'James', age: 24, job: 'Designer'},
+    { name: 'John', age: 35, job: 'Software Evangelist'},
+    { name: 'Jhonny', age: 29, job: 'Developer'},
+  ]
+  headers = [
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Age'},
+    { key: 'job', label: 'Job'},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  //data and headers will be passed down to the child
 
 }
