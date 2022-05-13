99e0f2e631a0edddbd07d8adcdf4d146755f4370
Sergio <webdev.slara@gmail.com>
2022-05-13 09:55:50 +0200

    created ModsHome and Modal components inside mods module, imported Sharedmodule as well

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule} from '../shared/shared.module';
 import { ModsRoutingModule } from './mods-routing.module';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
+import { ModalComponent } from './modal/modal.component';
 
 //created with ng g m Elements --routing
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModsHomeComponent,
+    ModalComponent
+  ],
   imports: [
     CommonModule,
-    ModsRoutingModule
+    ModsRoutingModule,
+    SharedModule
   ]
 })
 export class ModsModule { }
